79fa5bb3a5eaa24b3a960b01a528b8ad2ddc5520
DanielSuarez <danielsuarezdev@gmail.com>
2021-12-13 13:40:57 -0500

    pre npm

--- src/02-componets-patterns/pages/ShoppingPages.tsx
+++ src/02-componets-patterns/pages/ShoppingPages.tsx
@@ -17,22 +17,17 @@ export const ShoppingPages = () => {
       <hr />
       <ProductCard
         product={product}
-        className="bg-dark text-white"
         key={product.id}
         initialValue={{
           count: 0,
           maxCount: 10,
         }}
       >
-        {({ reset, count, maxCount }) => (
+        {() => (
           <>
-            <ProductImages className="custom-image" />
+            <ProductImages />
             <ProductTitle />
-            <ProductButtons className="custom-button" />
-            <div>
-              {count === maxCount ? <p>Llegaste al limite permitido</p> : <></>}
-            </div>
-            {count > 0 && <button onClick={reset}>Reset</button>}
+            <ProductButtons />
           </>
         )}
       </ProductCard>
